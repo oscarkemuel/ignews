6da911b5f03a39dab0b71349532a1a4eaec675e6
Oscar Kemuel <oscar.kemuel5@gmail.com>
2022-07-04 16:38:32 -0300

    feat: initial setting

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>hello world</h1>
+    </>
+  )
+}
+
+export default Home
