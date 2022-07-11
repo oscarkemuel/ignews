4dd07cebf7a2d6abd0588eb7f58e84e0cf21c4fd
Oscar Kemuel <oscar.kemuel5@gmail.com>
2022-07-11 06:19:07 -0300

    feat: styling home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>hello world</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to tall the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
