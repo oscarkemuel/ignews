5bc4049aa6ec13769f11b891b791ba712bc975a5
Oscar Kemuel <oscar.kemuel5@gmail.com>
2022-07-11 05:39:06 -0300

    feat: add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from './styles.module.scss';
+
+export function Header(){
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Logo" />
+        <nav>
+          <a href="#" className={styles.active}>Home</a>
+          <a href="#">Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
